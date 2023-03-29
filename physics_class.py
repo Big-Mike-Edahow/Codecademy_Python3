2e377de37fc1f766e6592757199fd0e3d1381c8a
Mike Jackson <perigran_falcon@yahoo.com>
2023-03-29 16:56:43 -0600

    Add files via upload

---
+++ physics_class.py
@@ -0,0 +1,48 @@
+# physics_class.py
+"""
+You are a physics teacher preparing for the upcoming semester.
+You want to provide your students with some functions that will
+help them calculate some fundamental physical properties.
+"""
+
+# Variable initialization for "Use the Force" section
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
+c = 3*10**8
+
+def f_to_c(f_temp):
+  c_temp = (f_temp -32) * (5/9)
+  return c_temp
+
+f100_in_celsius = f_to_c(100)
+
+def c_to_f(c_temp):
+  f_temp = c_temp * (9/5) + 32
+  return f_temp
+
+c0_in_fahrenheit = c_to_f(0)
+
+# "Use the Force" section
+def get_force(mass, acceleration):
+  return mass * acceleration
+
+def get_energy(mass, c):
+  return mass * (c * c)
+
+def get_work(mass, acceleration, distance):
+  force = get_force(mass, acceleration)
+  return force * distance
+
+train_force = get_force(train_mass, train_acceleration)
+
+print("The GE train supplies ", train_force, "Newtons of force.\n")
+
+bomb_energy = get_energy(bomb_mass, c)
+
+print("A 1kg bomb supplies ", bomb_energy, "Joules.\n")
+
+train_work = get_work(train_mass, train_acceleration, train_distance)
+
+print("The GE train does ", train_work, " Joules of work over ", train_distance, " meters.")
