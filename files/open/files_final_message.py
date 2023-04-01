86fcb4793b6a113d5fa7f1f3f3319af29674df38
Mike Jackson <perigran_falcon@yahoo.com>
2023-03-31 18:10:30 -0600

    Add files via upload

---
+++ files/open/files_final_message.py
@@ -0,0 +1,9 @@
+# files_final_message.py
+"""
+You have all the skills necessary to read,
+write, and update files programmatically,
+a very useful skill in the Python universe!
+"""
+
+with open('file.txt') as file_object:
+  print(file_object.read())
