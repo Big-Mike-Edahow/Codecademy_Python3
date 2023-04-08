4e13bdc2fc2d94adbc97780cb73c08a0450365d0
Mike Jackson <perigran_falcon@yahoo.com>
2023-04-08 12:14:38 -0600

    Add files via upload

---
+++ projects/multiple_returns.py
@@ -0,0 +1,13 @@
+# multiple_returns.py
+
+def top_tourist_locations_italy():
+  first = "Rome"
+  second = "Venice"
+  third = "Florence"
+  return first, second, third 
+  
+most_popular1, most_popular2, most_popular3 = top_tourist_locations_italy()
+
+print(most_popular1)
+print(most_popular2)
+print(most_popular3)
